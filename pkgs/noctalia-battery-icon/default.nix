53eaad9dd569a39ab27e8f7f2d056f40e8ceac22
Philipp Schmitt <philipp@schmitt.co>
2026-09-05 19:25:29 +0200

    battery-icon: control supported fans from power panel

--- pkgs/noctalia-battery-icon/default.nix
+++ pkgs/noctalia-battery-icon/default.nix
@@ -13,7 +13,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "noctalia-battery-icon";
-  version = "0.5.0";
+  version = "0.5.1";
 
   src = lib.fileset.toSource {
     root = ../../plugins/battery-icon;
