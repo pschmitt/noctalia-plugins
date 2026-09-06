effa86df94c6af264c31b7eab75f32ce220acc99
Philipp Schmitt <philipp@schmitt.co>
2026-09-06 10:15:49 +0200

    battery-icon: detect UPower device kinds

--- pkgs/noctalia-battery-icon/default.nix
+++ pkgs/noctalia-battery-icon/default.nix
@@ -13,7 +13,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "noctalia-battery-icon";
-  version = "0.5.3";
+  version = "0.5.4";
 
   src = lib.fileset.toSource {
     root = ../../plugins/battery-icon;
