3f77235cccd3507da74eb23fbc3ad8e8daddbfc6
Philipp Schmitt <philipp@schmitt.co>
2026-09-06 15:29:43 +0200

    battery-icon: choose panel size by content

--- pkgs/noctalia-battery-icon/default.nix
+++ pkgs/noctalia-battery-icon/default.nix
@@ -13,7 +13,7 @@
 
 stdenvNoCC.mkDerivation {
   pname = "noctalia-battery-icon";
-  version = "0.5.10";
+  version = "0.5.12";
 
   src = lib.fileset.toSource {
     root = ../../plugins/battery-icon;
